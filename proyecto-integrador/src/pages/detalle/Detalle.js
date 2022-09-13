4769993ceffb6abb91f59c26f3593d15be94e316
rodrigovgnUdesa <rvacaguzmannacif@udesa.edu.ar>
2022-09-13 10:05:07 -0300

    mejorando detalle

--- proyecto-integrador/src/pages/detalle/Detalle.js
+++ proyecto-integrador/src/pages/detalle/Detalle.js
@@ -1,30 +1,52 @@
 import React, {Component} from 'react'
+import "./Detalle.css"
 
- export default class DetallePelicula extends Component {
+ export default class Detalle extends Component {
     constructor(props) {
         super(props)
         this.state={
             id: this.props.match.params.id,
-            detalle: {} 
+            detalle: {},
+            /* boton: JSON.parse(localStorage.getItem('favoritos')).some((fav)=> fav.id === this.props.peliculas.id) */
         }
     }
 
 componentDidMount(){
-        fetch(`'https://api.themoviedb.org/3/movie/${this.state.id}?api_key=c0945689b0a582e110971301d6ea8be2&language=es'`)
+        fetch(`https://api.themoviedb.org/3/movie/${this.state.id}?api_key=c0945689b0a582e110971301d6ea8be2&language=es`)
         .then(res => res.json())
         .then(data => {
             console.log (data)
                 return this.setState({
-                    datalle : data
+                    detalle : data
                 })
         })
         .catch(err => console.log(err))
 }
 
+/* handleButton(){
+    this.setState({boton: !this.state.boton}, ()=>{this.props.favorito(this.props.peliculas)})
+} */
+
 render(){
+
+    let img = 'https://image.tmdb.org/t/p/w342'
+    
     return (
     <>
-        <img src={this.state.detalle.image}/>
+    <h1>Detalle de {this.state.detalle.title}</h1>
+    <section className='cardContainer'>
+        <article className='item-card-detail'>
+            <img src={`${img}${this.state.detalle.poster_path}`}alt="imagen" />
+            <p>Rating: {this.state.detalle.vote_average}</p>
+            <p>Fecha de estreno: {this.state.detalle.release_date}</p>
+            <p>Duración: {this.state.detalle.runtime} minutos</p>
+            <p>Sinópsis: {this.state.detalle.overview}</p>
+            {/* <p>Género: {this.state.detalle.genres}</p> */}
+            {/* <button className='buttonFav' onClick={()=> this.handleButton()}>{this.state.boton ? 'Quitar' : 'Agregar'}</button> */}
+        </article>
+        
+    </section>
+        
     </>
     )
 }
